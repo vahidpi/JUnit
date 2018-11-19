f01c4fd86e00d0b6128806eca667a9b6ffc2483b
Vahid Piroozbakht <vahid.piroozbakht@paf.com>
2018-11-19 16:05:10 +0200

    /21

--- unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
+++ unit-testing/src/main/java/com/in28minutes/unittesting/unittesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.in28minutes.unittesting.unittesting;
+package com.in28minutes.unittesting.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
